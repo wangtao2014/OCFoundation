0f18fa0762cbf5cf97e37e334cdf5ddb3e03187e
wangtao <umpay@wangtao.local>
2014-03-25 18:00:19 +0800

    OCFoundation
    OC Foundation API

---
+++ OCFoundation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OCFoundation
+//
+//  Created by wangtao on 14-3-25.
+//  Copyright (c) 2014年 wangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
